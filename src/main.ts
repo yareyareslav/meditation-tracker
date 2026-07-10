548a6ba2d214f0ffde2e086669c819ca8dc51540
Makanin Yaroslav <fobosguychanel@gmail.com>
2026-07-10 16:58:09 +0300

    feat: store for cards

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "./base.css"
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+
+app.mount('#app')
---
+++ src/typescript/meditation.types.ts
@@ -0,0 +1,14 @@
+export type Meditation = {
+  created_at: string
+  updated_at: string
+  id: number
+  title: string
+  description: string
+  duration_min: number
+}
+
+export type MeditationCardProps = {
+  title: Meditation['title']
+  description: Meditation['description']
+  duration: Meditation['duration_min']
+}
